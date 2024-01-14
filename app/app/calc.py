c2a83a04c65bf050836604a7c46fea15f7996c04
Anindya Mukherjee <andysenclave@gmail.com>
2024-01-14 20:19:15 +0530

    feat: adding first code and tests

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator fuctions for testing
+"""
+
+
+def add(x, y):
+    """Adds two numbers and returns result"""
+    return x + y
+
+def subtract(x, y):
+    """Subtract two numbers and returns result"""
+
+    return y - x
